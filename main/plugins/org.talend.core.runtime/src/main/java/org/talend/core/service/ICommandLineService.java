fe5f59725a37b0e06a7ca45a07a40cfac69026f9
hcyi <hcyi@talend.com>
2018-05-11 18:22:45 +0800

    feat(TUP-20033):Audit in project settings in studio.

---
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/service/ICommandLineService.java
@@ -0,0 +1,29 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2018 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.core.service;
+
+import org.talend.core.IService;
+
+/**
+ * 
+ * created by hcyi on May 10, 2018 Detailled comment
+ *
+ */
+public interface ICommandLineService extends IService{
+
+    public void initEMFQuery();
+
+    public void populateAudit(String input);
+
+    public void generateAuditReport(String input);
+}
